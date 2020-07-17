260d5fe45b39d4eecfbd98c124ae6a81dc26aad7
tonytiratayoclock <tony@oclock.io>
2020-07-17 12:00:10 +0200

    Add todo done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 // == Import npm
-import React from "react";
-
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 // == Import
 import Form from "../Form";
 import Counter from "../Counter";
@@ -9,14 +9,68 @@ import "./styles.css";
 
 import todosData from "../../data/tasks";
 
-// == Composant
-const App = () => (
-  <div className="app">
-    <Form />
-    <Counter number={0} />
-    <Tasks list={todosData} />
-  </div>
-);
+class App extends Component {
+  state = {
+    newTodoText: "test",
+    todos: todosData,
+  };
+
+  changeTodoText = (texteTodo) => {
+    // Je modifie mon state
+    // pour changer le texte de l'input
+    this.setState({
+      newTodoText: texteTodo,
+    });
+  };
+
+  addTodo = () => {
+    // Je récupère le texte
+    const { newTodoText } = this.state;
+    // Je fabrique l'objet todo à rajouter
+    const newTodo = {
+      id: uuidv4(),
+      label: newTodoText,
+      done: false,
+    };
+    // Lorsque je modofie le state,
+    // je NE DOIS JAMAIS modifier
+    // le state directement
+    // this.state.truc = "quelquechose"
+    // La ligne ci dessus VIOLE le principe
+    // fondamental de la programmation fonctionnelle
+    // UNE FONCTION NE MODIFIE JAMAIS ELLE MEME
+    // UNE VALEUR QU'ELLE REÇOIT
+    const newTodos = [newTodo, ...this.state.todos];
+    this.setState({
+      todos: newTodos,
+      newTodoText: "",
+    });
+  };
+
+  render() {
+    // J'extraie les todos du state
+    const { todos, newTodoText } = this.state;
+    return (
+      <div className="app">
+        <Form
+          text={newTodoText}
+          onTodoSubmit={this.addTodo}
+          changeText={this.changeTodoText}
+        />
+        <Counter number={0} />
+        <Tasks list={todos} />
+      </div>
+    );
+  }
+}
+
+// Transformer App en class
+// lui donner un state
+// mettre la liste des todos dans le state
+// Rajouter une todo dans le state depuis composant Form
+// Form doit recevoir une méthode qui vient de App
+// La méthode dans App reçoive un texte de todo
+// Avec le texte, la méthode rajoute une todo dans le state
 
 // == Export
 export default App;
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,13 +2,27 @@ import React from "react";
 
 import "./style.scss";
 
-const Form = () => {
+const Form = (props) => {
+  const { text, changeText, onTodoSubmit } = props;
   return (
-    <form className="todo-form">
+    <form
+      className="todo-form"
+      onSubmit={(evt) => {
+        evt.preventDefault();
+        console.log("submit");
+        onTodoSubmit();
+      }}
+    >
       <input
         type="text"
         placeholder="Ajoutez une tâche"
         className="todo-input"
+        value={text}
+        onChange={(evt) => {
+          console.log(evt.target.value);
+          // PRevenir le state de son nouvel état
+          changeText(evt.target.value);
+        }}
       />
     </form>
   );
